f3e9dc521460e4ca53030d146ec0997c6a665209
Prateek Gogia <prateek.gogia@razorpay.com>
2021-09-24 12:39:40 +0530

    24 September session end result

--- src/App.js
+++ src/App.js
@@ -1,23 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer } from 'react';
+
+const EMAIL_CHANGE = 'EMAIL_CHANGE';
+const PASSWORD_CHANGE = 'PASSWORD_CHANGE';
+const FIRSTNAME_CHANGE = 'FIRSTNAME_CHANGE';
+const LASTNAME_CHANGE = 'LASTNAME_CHANGE';
+
+const formReducer = (state, action) => {
+  switch (action.type) {
+    case EMAIL_CHANGE:
+      return { ...state, email: action.payload };
+    case PASSWORD_CHANGE:
+      return { ...state, password: action.payload };
+    case FIRSTNAME_CHANGE:
+      return { ...state, firstName: action.payload };
+    case LASTNAME_CHANGE:
+      return { ...state, lastName: action.payload };
+    default:
+      return state;
+  }
+};
 
 function App() {
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const { email, password, firstName, lastName } = state;
+    let data = {
+      email: email,
+      password: password,
+      firstName: firstName,
+      lastName: lastName,
+    };
+    console.log('data', data);
+  };
+
+  const intialState = {
+    email: '',
+    password: '',
+    firstName: '',
+    lastName: '',
+  };
+
+  const fields = ['email', 'password', 'firstName', 'lastName'];
+
+  const [state, dispatch] = useReducer(formReducer, intialState);
+
+  const onChange = (e) => {
+    switch (e.target.name) {
+      case 'email':
+        dispatch({
+          type: EMAIL_CHANGE,
+          payload: e.target.value,
+        });
+        break;
+      case 'password':
+        dispatch({
+          type: PASSWORD_CHANGE,
+          payload: e.target.value,
+        });
+        break;
+      case 'firstName':
+        dispatch({
+          type: FIRSTNAME_CHANGE,
+          payload: e.target.value,
+        });
+        break;
+      case 'lastName':
+        dispatch({
+          type: LASTNAME_CHANGE,
+          payload: e.target.value,
+        });
+        break;
+      default:
+        return state;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={onSubmit}>
+        <div>
+          {fields.map((field) => {
+            return (
+              <label htmlFor={field}>
+                <input
+                  type={field}
+                  name={field}
+                  value={state[field]}
+                  placeholder=""
+                  onChange={onChange}
+                />
+              </label>
+            );
+          })}
+          <button type="submit">Submit</button>
+        </div>
+      </form>
     </div>
   );
 }
